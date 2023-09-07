7d570567b3cecf3c811088424df4715c5c83d23b
Sobhan Bera <sobhanbera258@gmail.com>
2023-09-07 19:39:21 +0000

    customized test website

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,24 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
-    description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+    title: "Portfolio Website",
+    description: "Potfolio website version 4",
+    url: "https://sobhanbera.github.io",
   },
   {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+    title: "Portfolio Website",
+    description: "Potfolio website version 4",
+    url: "https://sobhanbera.github.io",
   },
   {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    title: "Portfolio Website",
+    description: "Potfolio website version 4",
+    url: "https://sobhanbera.github.io",
   },
   {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    title: "Portfolio Website",
+    description: "Potfolio website version 4",
+    url: "https://sobhanbera.github.io",
   },
 ];
 
